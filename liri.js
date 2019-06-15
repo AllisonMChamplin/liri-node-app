54a413eae90129a508be49790c1d0672978edafd
Allison Champlin <47014575+AllisonMChamplin@users.noreply.github.com>
2019-06-14 22:51:27 -0500

    Added Bands in Town Axios search/results

--- liri.js
+++ liri.js
@@ -79,6 +79,75 @@ if (command === "movie-this") {
             console.log(error.config);
         });
 
+} else if (command === "concert-this") {
+    // console.log("concert stuff here");
+    // Create an empty variable for holding the band name
+    var bandName = "";
+    // Loop through all the words in the node argument
+    // And include "+"s
+    for (var i = 0; i < input.length; i++) {
+        if (i < input.length) {
+            bandName = bandName + input[i];
+        } else {
+            bandName += input[i];
+        }
+    }
+
+    if (bandName === "") {
+        console.log("You didn't enter a band name.");
+    } else {
+        // Then run a request with axios to the OMDB API with the movie specified
+        var queryUrl = "https://rest.bandsintown.com/artists/" + bandName + "/events?app_id=codingbootcamp";
+    }
+
+    // This line is just to help us debug against the actual URL.
+    console.log(queryUrl);
+
+    axios.get(queryUrl).then(
+        function (response) {
+            if (!response.data.length > 0) {
+                console.log("No upcoming events");
+            } else {
+            for (i = 0; i < response.data.length; i++) {
+                console.log("ARTIST: ", bandName);
+                console.log("VENUE: ", response.data[i].venue.name);
+                console.log("LOCATION: ", response.data[i].venue.city + ', ' + response.data[i].venue.country);
+                // console.log("Title: " + response.data.Title);
+                // console.log("Release Year: " + response.data.Year);
+                // console.log("IMDB Rating: " + response.data.Ratings[0].Value);
+                // console.log("Rotten Tomatoes Rating: " + response.data.Ratings[1].Value);
+                // console.log("Country: " + response.data.Country);
+                // console.log("Language: " + response.data.Language);
+                // console.log("Plot: " + response.data.Plot);
+                // console.log("Actors: " + response.data.Actors);
+            }
+        }
+        })
+        .catch (function (error) {
+                if (error.response) {
+                    // The request was made and the server responded with a status code
+                    // that falls out of the range of 2xx
+                    console.log("---------------Data---------------");
+                    console.log(error.response.data);
+                    console.log("---------------Status---------------");
+                    console.log(error.response.status);
+                    console.log("---------------Status---------------");
+                    console.log(error.response.headers);
+                } else if (error.request) {
+                    // The request was made but no response was received
+                    // `error.request` is an object that comes back with details pertaining to the error that occurred.
+                    console.log(error.request);
+                } else {
+                    // Something happened in setting up the request that triggered an Error
+                    console.log("Error", error.message);
+                }
+                console.log(error.config);
+            });
+
+
+
+
+
 } else if (command === "spotify-this-song") {
     // console.log("spot");
     // Create an empty variable for holding the song name
